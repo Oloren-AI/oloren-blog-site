4e44b0e3920fda9cd174a62422dcf4ed168a96b5
Raunak Chowdhuri <sauhaarda@gmail.com>
2022-08-02 17:02:26 -0400

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_posts/*.html",
+    "./*.html",
+  ],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['"Inter"', "serif"],
+      body: ["Inter", "serif"],
+      display: ["Inter", "serif"],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: [
+      {
+        light: {
+          primary: "#1D62E7",
+          secondary: "#6943D8",
+          accent: "#48DB72",
+          neutral: "#ECECEC",
+          "base-100": "#FFFFFF",
+          info: "#FFFFFF",
+          success: "#48DB72",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
+};
